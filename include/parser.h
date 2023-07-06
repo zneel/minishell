c5ed4b63bfb4e6f5ed2d62bd39f2ad8a0f98f480
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-07-06 10:45:39 +0200

    before rebase

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:25:00 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/24 19:00:00 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/07/24 19:00:20 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 # define PARSER_H
 
 # include "lexer.h"
+# include "lists.h"
 # include "stack.h"
 
 typedef struct s_node	t_node;
+
+typedef enum e_error
+{
+	NO_ERROR = (1U << 0),
+	SYNTAX = (1U << 1),
+	FATAL = (1U << 2),
+}						t_error;
 
 typedef enum e_node_type
 {
@@ -46,6 +54,7 @@ typedef struct s_parser
 	long				parse_state;
 	t_stack				*parse_stack;
 	t_node				*root;
+	t_error				error;
 }						t_parser;
 
 #endif

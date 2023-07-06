813613319f88bcc876ac34a78c01159280c8b5a0
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-07-06 10:49:44 +0200

    compile after rebase

--- src/main.c
+++ src/main.c
@@ -3,86 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhoyer <mhoyer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 21:30:45 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/24 18:03:21 by mhoyer           ###   ########.fr       */
+/*   Updated: 2023/07/24 19:01:19 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-#include "exec.h"
 #include "ft_printf.h"
 #include "lexer.h"
 #include "minishell.h"
 
-char    *node_type_to_str(t_node_type type)
-{
-    switch (type)
-    {
-    case COMMAND:
-        return ("COMMAND");
-    case PIPE:
-        return ("PIPE");
-    case AND:
-        return ("AND");
-    case OR:
-        return ("OR");
-    case LESS:
-        return ("LESS");
-    case GREAT:
-        return ("GREAT");
-    case DGREAT:
-        return ("DGREAT");
-    case HERE_DOC:
-        return ("HERE_DOC");
-    default:
-        return ("UNKNOWN");
-    }
-}
-
-void    pretty_print_ast(t_node *node, char *prefix)
-{
-    int        len;
-    char    *new_prefix;
-
-    if (node == NULL)
-        return ;
-    printf("%s├── Type: %s\n", prefix, node_type_to_str(node->type));
-    if (node->data != NULL)
-        printf("%s|   ├── Command: %s\n", prefix, node->data);
-    len = strlen(prefix);
-    new_prefix = malloc(len + 5);
-    strcpy(new_prefix, prefix);
-    strcat(new_prefix, node->right != NULL ? "|   " : "    ");
-    if (node->left != NULL)
-    {
-        printf("%s|   └── Left:\n", prefix);
-        pretty_print_ast(node->left, new_prefix);
-    }
-    else
-        printf("%s|   └── Left: NULL\n", prefix);
-    if (node->right != NULL)
-    {
-        printf("%s└── Right:\n", prefix);
-        pretty_print_ast(node->right, new_prefix);
-    }
-    else
-        printf("%s└── Right: NULL\n", prefix);
-    free(new_prefix);
-}
-
 int	main(int argc, char **argv, char **env)
 {
 	t_minishell	minishell;
-    t_lexer     *lexed;
-    char        *line;
+	t_lexer		*lexed;
+	char		*line;
 
-	
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 	minishell.env = env_cpy(env);
-    minishell.pids = NULL;
+	minishell.pids = NULL;
 	while (1)
 	{
 		line = readline("minishell> ");

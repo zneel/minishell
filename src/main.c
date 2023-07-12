0749fc5c02ba275b9bf4bd0b25fb3d07a73be858
mhoyer <mathieuhoyer@gmail.com>
2023-07-12 10:12:20 +0200

    ajout de gestion de \t

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mhoyer <mhoyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 21:30:45 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/11 12:29:10 by mhoyer           ###   ########.fr       */
+/*   Updated: 2023/07/12 10:11:32 by mhoyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv, char **env)
 		line = readline("minishell> ");
 		if (!line)
 			return (1);
-		if (line && *line && *line != ' ')
+		if (line && *line && *line != ' ' && *line != '\t')
 			add_history(line);
 		lexed = lexer(line);
 		debug_lexer(lexed);

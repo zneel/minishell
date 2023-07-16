a220f9efb606c2f204cb8a58b92b6ebd963ee19a
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-07-16 21:14:37 +0200

    wip

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -6,12 +6,26 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 11:33:20 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/06 11:33:55 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/07/13 09:03:38 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-t_node	*ast_new_node(t_node *node, t_node *left, t_node *right)
+void	ast_attach_binary(t_node *root, t_node *left, t_node *right)
 {
+	if (!root)
+		return ;
+	root->left = left;
+	root->right = right;
+}
+
+void	ast_delete(t_node *node)
+{
+	if (!node)
+		return ;
+	free(node->raw_command);
+	ast_delete(node->left);
+	ast_delete(node->right);
+	free(node);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,62 +6,45 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:05:49 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/06 10:49:17 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/07/13 09:52:10 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
-
-void	shift(t_parser *parser)
-{
-	if (parser->current_tok->type == T_WORD)
-		stack_push(&parser->parse_stack, parser->current_tok);
-}
 
 void	set_parser_error(t_parser *parser, t_error error)
 {
 	parser->error |= error;
 }
 
-void	reduce(t_parser *parser)
+int	accept(t_parser *parser, t_type type)
 {
-	t_token	*top;
-	t_list	*new;
-
-	top = stack_peek(parser->parse_stack);
-	if (parser->current_tok->type == T_PIPE
-		|| parser->current_tok->type == T_AND
-		|| parser->current_tok->type == T_OR
-		|| parser->current_tok->type == T_DGREAT
-		|| parser->current_tok->type == T_DLESS
-		|| parser->current_tok->type == T_GREAT
-		|| parser->current_tok->type == T_LESS)
-		while (top)
-		{
-			top = stack_peek(parser->parse_stack);
-			new = ft_lstnew(top->value);
-			if (!new)
-				return (set_parser_error(parser, FATAL));
-			ft_lstadd_back(&parser->root->raw_command, new);
-			stack_pop(&parser->parse_stack, (void (*)(void *))delete_token);
-		}
+	if (type == parser->current_tok->type)
+	{
+		parser->current_tok = parser->current_tok->next;
+		return (1);
+	}
+	return (0);
 }
 
-void	consume_token(t_parser *parser, t_lexer *lexer)
+int	peek(t_parser *parser, t_type type)
 {
-	if (lexer && lexer->token_lst)
-	{
-		if (lexer->token_lst->next)
-			parser->current_tok = lexer->token_lst;
-	}
+	if (type == parser->current_tok->next->type)
+		return (1);
+	return (0);
+}
+
+int	expect(t_parser *parser, t_type type)
+{
+	if (accept(parser, type))
+		return (1);
+	set_parser_error(parser, SYNTAX_ERROR);
+	return (0);
 }
 
 int	init_parser(t_parser *parser)
 {
 	parser->current_tok = NULL;
-	parser->parse_stack = malloc(sizeof(t_stack));
-	if (!parser->parse_stack)
-		return (0);
 	parser->root = NULL;
 	parser->parse_state = 0;
 	parser->error = NO_ERROR;
@@ -72,8 +55,9 @@ t_node	*parse(t_lexer *lexer)
 {
 	t_parser	parser;
 
-	if (!lexer && !init_parser(&parser))
+	if (!lexer)
 		return (NULL);
-	consume_token(&parser, lexer);
+	init_parser(&parser);
+	parse_group(&parser);
 	return (parser.root);
 }

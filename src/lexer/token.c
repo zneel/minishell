34771f89b34ab65a7a7e29a3a6082a03ab79dba8
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-07-24 14:24:06 +0200

    correct lexer special char

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -6,11 +6,10 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:16:45 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/05 16:49:58 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/07/24 13:42:15 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include "lexer.h"
 #include "libft.h"
 
@@ -21,14 +20,12 @@ t_token	*new_token(t_type type, char *value)
 	token = malloc(sizeof(t_token));
 	if (!token)
 		return (NULL);
-	if (!value)
-	{
-		free(token);
-		return (NULL);
-	}
 	token->type = type;
 	token->value = value;
-	token->len = ft_strlen(value);
+	if (value)
+		token->len = ft_strlen(value);
+	else
+		token->len = 0;
 	token->next = NULL;
 	return (token);
 }
@@ -37,7 +34,11 @@ void	add_token(t_lexer *lexer, t_token *token)
 {
 	t_token	*curr;
 
-	ft_printf("adding token:%s\n", token->value);
+	if (!token->value)
+		printf("ADD TOKEN TYPE=%s VALUE=NULL\n", token_to_str(token->type));
+	else
+		printf("ADD TOKEN TYPE=%s VALUE=%s\n", token_to_str(token->type),
+				token->value);
 	curr = lexer->token_lst;
 	if (!curr)
 		lexer->token_lst = token;

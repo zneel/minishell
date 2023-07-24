d62f33b36fcdac5b635a55e89346fc7f102cca9a
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-07-24 19:14:30 +0200

    execute tree

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 21:30:45 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/07/24 19:07:05 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/07/24 19:13:18 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-#include "get_next_line.h"
+#include "exec.h"
 #include "lexer.h"
 #include "lists.h"
 #include "minishell.h"
@@ -33,13 +33,14 @@ int	main(int argc, char **argv, char **env)
 		line = readline("minishell> ");
 		if (!line)
 			return (1);
-		if (line && *line && *line != ' ' && *line != '\t')
+		if (line && *line && !ft_isspace(*line))
 			add_history(line);
 		lexed = lexer(line);
 		free(line);
 		debug_lexer(lexed);
 		root = parse(lexed);
 		delete_lexer(lexed);
+		prep_exec(root, &minishell);
 		ast_delete(root);
 	}
 	free_minishell(&minishell);

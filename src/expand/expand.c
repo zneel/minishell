3dc24d99ab1afdbee73ee827f23d9039f3807c2e
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-08-24 14:06:39 +0200

    norm

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -1,111 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 14:04:12 by ebouvier          #+#    #+#             */
+/*   Updated: 2023/08/24 14:04:12 by ebouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "expand.h"
-#include "minishell.h"
-
-char	*ft_realloc(char *ptr, size_t size)
-{
-	char	*new_ptr;
-
-	new_ptr = ft_calloc(size + 1, sizeof(char));
-	if (!new_ptr)
-		return (NULL);
-	if (ptr)
-	{
-		ft_strlcpy(new_ptr, ptr, size + 1);
-		free(ptr);
-	}
-	return (new_ptr);
-}
-
-void	free_expand_var(t_expand_var *var)
-{
-	free(var->name);
-	free(var);
-}
-
-t_bool	valid_expand_char(char c)
-{
-	return (ft_isalnum(c) || c == '_');
-}
-
-char	*get_end_variable(char *input)
-{
-	int	i;
-
-	i = 0;
-	while (input[i])
-	{
-		if (input[i] && !valid_expand_char(input[i]))
-			break ;
-		i++;
-	}
-	return (input + i);
-}
-
-t_expand_var	*get_variable(t_minishell *minishell, char *input)
-{
-	t_expand_var	*var;
-
-	var = malloc(sizeof(t_expand_var));
-	if (!var)
-		return (NULL);
-	var->end = get_end_variable(input);
-	var->name = ft_substr(input, 0, var->end - input);
-	var->value = get_env(minishell, var->name);
-	return (var);
-}
-
-void	expand_last_status(t_expand_str *expand, t_minishell *minishell)
-{
-	char	*last_status;
-
-	last_status = ft_itoa(minishell->last_status);
-	expand->result_size += ft_strlen(last_status);
-	expand->result = ft_realloc(expand->result, expand->result_size);
-	ft_strlcpy(expand->result + expand->i, last_status, ft_strlen(last_status)
-		+ 1);
-	expand->i += ft_strlen(last_status);
-	expand->input++;
-	free(last_status);
-}
-
-void	handle_dollar(t_expand_str *expand, t_minishell *minishell)
-{
-	t_expand_var	*var;
-
-	expand->input++;
-	var = get_variable(minishell, expand->input);
-	if (var->value)
-	{
-		expand->result_size += ft_strlen(var->value);
-		expand->result = ft_realloc(expand->result, expand->result_size);
-		ft_strlcpy(expand->result + expand->i, var->value, ft_strlen(var->value)
-			+ 1);
-		expand->i += ft_strlen(var->value);
-		expand->input += ft_strlen(var->name);
-	}
-	else
-	{
-		if (*expand->input == '?')
-			expand_last_status(expand, minishell);
-		else
-			expand->input = var->end;
-	}
-	free_expand_var(var);
-}
-
-static void	copy_and_increment(t_expand_str *expand)
-{
-	expand->result = ft_realloc(expand->result, expand->result_size + 1);
-	*(expand->result + expand->i++) = *expand->input;
-	expand->result_size++;
-	expand->input++;
-}
-
-static void	change_state(unsigned int new_state, t_expand_str *expand)
-{
-	expand->state ^= new_state;
-	expand->input++;
-}
 
 void	handle_expand(t_expand_str *expand, t_minishell *minishell)
 {

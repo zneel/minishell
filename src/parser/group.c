67951caec95d51c540bdd9158edb742e3007344f
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-08-29 21:11:45 +0200

    fix paren leaks

--- src/parser/group.c
+++ src/parser/group.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 13:14:19 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/08/18 13:16:08 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/08/29 21:07:45 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ t_node	*group(t_parser *parser)
 	node = command_line(parser);
 	if (!node)
 		return (NULL);
-	if (!accept(parser, T_RPAREN))
+	if (!peek(parser, T_RPAREN))
 		return (ast_delete(node), NULL);
+	accept(parser, T_RPAREN);
+	stack_pop(&parser->parse_stack, free);
 	return (node);
 }

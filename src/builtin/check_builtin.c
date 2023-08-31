869fc54c17fe422fa83432d79c9a7da360733081
mhoyer <mathieuhoyer@gmail.com>
2023-08-31 16:03:10 +0200

    builtin = no path

--- src/builtin/check_builtin.c
+++ src/builtin/check_builtin.c
@@ -6,26 +6,11 @@
 /*   By: mhoyer <mhoyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 21:51:16 by mhoyer            #+#    #+#             */
-/*   Updated: 2023/08/30 18:51:53 by mhoyer           ###   ########.fr       */
+/*   Updated: 2023/08/31 15:58:44 by mhoyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-
-int	ft_strncmp_back(char *str, char *ex, int size)
-{
-	int	i;
-
-	i = ft_strlen(str);
-	while (i >= 0 && size >= 0 && str[i] == ex[size])
-	{
-		i--;
-		size--;
-	}
-	if (size == -1 && i != -1 && str[i] == '/')
-		return (0);
-	return (1);
-}
 
 t_builtin_type	check_npath(char *cmd)
 {
@@ -45,36 +30,5 @@ t_builtin_type	check_npath(char *cmd)
 		return (CD);
 	if (is_same(cmd, "exit"))
 		return (EXIT);
-	return (0);
-}
-
-t_builtin_type	check_wpath(char *cmd)
-{
-	if (!cmd)
-		return (NONE);
-	if (ft_strncmp_back(cmd, "echo", ft_strlen("echo")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "export", ft_strlen("export")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "env", ft_strlen("env")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "pwd", ft_strlen("pwd")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "unset", ft_strlen("unset")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "cd", ft_strlen("cd")) == 0)
-		return (W_PATH);
-	if (ft_strncmp_back(cmd, "exit", ft_strlen("exit")) == 0)
-		return (W_PATH);
-	return (0);
-}
-
-t_builtin_type	check_builtin(char *str)
-{
-	int	builtin;
-
-	builtin = check_npath(str);
-	if (builtin)
-		return (builtin);
-	return (check_wpath(str));
+	return (NONE);
 }

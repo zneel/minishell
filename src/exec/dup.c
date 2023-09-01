59fc131d17f0c4e2e1af39e2eee8fb2cd1bbfb39
Elliot Bouvier <ebouvier@student.42.fr>
2023-09-01 11:21:35 +0200

    fix-redirect-flags

--- src/exec/dup.c
+++ src/exec/dup.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 16:56:23 by mhoyer            #+#    #+#             */
-/*   Updated: 2023/08/31 19:55:35 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/09/01 11:18:58 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	dup_out(t_command *cmd)
 {
 	int	fdout;
 
-	if (cmd->mode & ~M_APPEND)
+	if (!(cmd->mode & M_APPEND))
 		fdout = open(cmd->file_out, O_WRONLY | O_TRUNC);
 	else
 		fdout = open(cmd->file_out, O_WRONLY | O_APPEND);
-	if (!fdout)
+	if (fdout == -1)
 		return (msg_error("No such file or directory", cmd->file_out, NULL));
 	dup2(fdout, STDOUT_FILENO);
 	close(fdout);
@@ -31,7 +31,7 @@ int	dup_in(t_command *cmd)
 {
 	int	fdin;
 
-	if (cmd->mode & ~M_HERE_DOC)
+	if (!(cmd->mode & M_HERE_DOC))
 		fdin = open(cmd->file_in, O_RDONLY, 0644);
 	else
 		fdin = open(FILE_HEREDOC, O_RDONLY, 0644);

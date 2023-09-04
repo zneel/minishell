71e0676aa48f5164572bcfea461d3ac067c9d24b
Mathieu Hoyer <mathieuhoyer@gmail.com>
2023-09-04 13:08:26 +0200

    echo -nn yes

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mhoyer <mhoyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 07:37:23 by mhoyer            #+#    #+#             */
-/*   Updated: 2023/08/30 20:02:12 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/09/04 13:07:45 by mhoyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+#include "builtin.h"
+
+t_bool	is_n(char *ext)
+{
+	int	i;
+
+	i = 1;
+	if (ext[0] != '-')
+		return (false);
+	while (ext[i] && ext[i] == 'n')
+		i++;
+	if (ext[i] == '\0')
+		return (true);
+	return (false);
+}
 
 int	echo(t_command *cmd)
 {
@@ -20,7 +36,7 @@ int	echo(t_command *cmd)
 	if (!cmd->command || !cmd->command[0])
 		return (1);
 	endl = 0;
-	if (cmd->command[1] && is_same(cmd->command[1], "-n"))
+	if (cmd->command[1] && is_n(cmd->command[1]))
 		endl = 1;
 	i = endl;
 	while (cmd->command[++i])
